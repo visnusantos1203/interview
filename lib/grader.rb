9a9f9a19b0eb3ff64bb0cf7104fd1671abce2e83
Dash <visnu.santos1203@gmail.com>
2025-04-09 16:32:16 +0800

    Add initializers and 'headers' method to Grader class

--- lib/grader.rb
+++ lib/grader.rb
@@ -1,7 +1,20 @@
+# frozen_string_literal: true
+
 require "csv"
 require "student"
 
 class Grader
   def initialize(file)
+    @file = CSV.read(file, headers: true)
+  def headers
+    excluded_headers = %w[first_name last_name age]
+    headers = @file.headers.reject { |header| excluded_headers.include?(header) }
+
+    headers.unshift("Name")
+    headers << "Average"
+
+    headers.map(&:capitalize)
+  end
+
   end
 end
--- spec/lib/grader_spec.rb
+++ spec/lib/grader_spec.rb
@@ -1,5 +1,19 @@
 RSpec.describe Grader do
-  it "should pass" do
-    expect(true).to eq(true)
+  let(:file) { File.open("./files/grades.csv") }
+  let(:grader) { Grader.new(file) }
+
+  describe "#initialize" do
+    it "should read the CSV file" do
+      expect(grader.instance_variable_get(:@file)).not_to be_nil
+      expect(grader.instance_variable_get(:@file).headers).to include("first_name", "last_name", "age", "english", "math", "physics")
+    end
+  end
+
+  describe "headers" do
+    it "should have filtered headers" do
+      expect(grader.headers).to include("Name", "English", "Math", "Physics", "Average")
+      expect(grader.headers).not_to include("first_name", "last_name", "age")
+    end
+  end
   end
 end
