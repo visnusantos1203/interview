aeb6d4f9ffa3ba3a40ce620e0b5cf9e05527baf5
Dash <visnu.santos1203@gmail.com>
2025-04-09 16:15:05 +0800

    Add initializer and 'full_name' method to Student class

--- lib/student.rb
+++ lib/student.rb
@@ -1,2 +1,15 @@
+# frozen_string_literal: true
+
 class Student
+  attr_accessor :first_name, :last_name, :age
+
+  def initialize(first_name, last_name, age)
+    @first_name = first_name
+    @last_name = last_name
+    @age = age
+  end
+
+  def full_name
+    "#{first_name} #{last_name}"
+  end
 end
--- spec/lib/student_spec.rb
+++ spec/lib/student_spec.rb
@@ -1,5 +1,17 @@
 RSpec.describe Student do
-  it "should fail" do
-    expect(true).to eq(false)
+  let (:student) { Student.new("John", "Doe", 20) }
+
+  describe "#initialize" do
+    it "should initialize with first name, last name, and age" do
+      expect(student.first_name).to eq("John")
+      expect(student.last_name).to eq("Doe")
+      expect(student.age).to eq(20)
+    end
+  end
+
+  describe "#full_name" do
+    it "should return the full name" do
+      expect(student.full_name).to eq("John Doe")
+    end
   end
 end
